ca4bb27a498cde9bc3f9410289f50a8fb20eae8b
hazuki <hazuki@live.be>
2020-07-21 16:04:24 +0200

    exception bij setLastname() aangemaakt

--- classes/User.php
+++ classes/User.php
@@ -1,92 +1,97 @@
 <?php
 
-    class User{
-        private $firstname;
-        private $lastname;
-        private $email;
-        private $password;
+class User
+{
+    private $firstname;
+    private $lastname;
+    private $email;
+    private $password;
 
-        /**
-         * Get the value of firstname
-         */ 
-        public function getFirstname()
-        {
-                return $this->firstname;
-        }
+    /**
+     * Get the value of firstname
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
 
-        /**
-         * Set the value of firstname
-         *
-         * @return  self
-         */ 
-        public function setFirstname($firstname)
-        {
-                if (empty($firstname)) {
-                    throw new Exception("Gelieve een achternaam in te vullen.");
-            } else {
-                    $this->firstname = $firstname;
+    /**
+     * Set the value of firstname
+     *
+     * @return  self
+     */
+    public function setFirstname($firstname)
+    {
+        if (empty($firstname)) {
+            throw new Exception("Gelieve een voornaam in te vullen.");
+        } else {
+            $this->firstname = $firstname;
 
-                    return $this;
-            }
-        }
-
-        /**
-         * Get the value of lastname
-         */ 
-        public function getLastname()
-        {
-                return $this->lastname;
-        }
-
-        /**
-         * Set the value of lastname
-         *
-         * @return  self
-         */ 
-        public function setLastname($lastname)
-        {
-                $this->lastname = $lastname;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of email
-         */ 
-        public function getEmail()
-        {
-                return $this->email;
-        }
-
-        /**
-         * Set the value of email
-         *
-         * @return  self
-         */ 
-        public function setEmail($email)
-        {
-                $this->email = $email;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of password
-         */ 
-        public function getPassword()
-        {
-                return $this->password;
-        }
-
-        /**
-         * Set the value of password
-         *
-         * @return  self
-         */ 
-        public function setPassword($password)
-        {
-                $this->password = $password;
-
-                return $this;
+            return $this;
         }
     }
+
+    /**
+     * Get the value of lastname
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set the value of lastname
+     *
+     * @return  self
+     */
+    public function setLastname($lastname)
+    {
+        if (empty($lastname)) {
+            throw new Exception("Gelieve een achternaam in te vullen.");
+        } else {
+            $this->lastname = $lastname;
+
+            return $this;
+        }
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
