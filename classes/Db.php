4e5b152108f5b773b22d222cf84dfe72b1cd0969
hazuki <hazuki@live.be>
2020-08-16 19:13:27 +0200

    Update Db.php
    DB conn aangepast met host values

--- classes/Db.php
+++ classes/Db.php
@@ -5,7 +5,7 @@
 
         public static function getConnection(){
             if(self::$conn === null){
-                self::$conn = new PDO('mysql:host=localhost;dbname=virtualcurrency', "root", "");
+                self::$conn = new PDO('mysql:host=localhost;dbname=hazuktr389_virtualcurrency', "hazuktr389_hazuktr389", "84hcmxht");
                 return self::$conn;
             }else{
                 return self::$conn;
